b574540e564184bec96c9aef533baaca58b6ebf0
Abhijith Surendran <abhijith.surendran@testingmavens.org>
2023-10-17 17:37:04 +0530

    send report

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,18 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "m3b8cw",
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports/mocha",
+    "reportFilename": "index",  // Specify your custom file name for the HTML report
+    overwrite: true,
+    html: true,
+    json: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require('cypress-mochawesome-reporter/plugin')(on);
+     // require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
