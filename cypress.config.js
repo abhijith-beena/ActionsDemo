62ca6e72654d4bea0622c914f8e4e08314b5ed69
Abhijith Surendran <abhijith.surendran@testingmavens.org>
2023-10-17 14:26:13 +0530

    fail

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    configFile: "reporterOpts.json"
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
