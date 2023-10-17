6dc6edd23a4f6ff64a060f13480e232fe258b83c
Abhijith Surendran <abhijith.surendran@testingmavens.org>
2023-10-17 17:50:51 +0530

    if not working, revert

--- sendReport.js
+++ sendReport.js
@@ -1,20 +1,21 @@
 const axios = require('axios');
 const fs = require('fs');
 
-const slackWebhookUrl = 'https://hooks.slack.com/services/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';  // Replace with your Slack webhook URL
-
+const slackWebhookUrl = process.argv[2];  // Replace with your Slack webhook URL
+const reportPath = 'cypress/reports/mocha/index.json';
 const sendMochawesomeReportToSlack = async () => {
   try {
     // Read the Mochawesome report
-    const report = fs.readFileSync('cypress/reports/mocha/index.json', 'utf-8');
+    const report = JSON.parse(fs.readFileSync(reportPath, 'utf8'));
 
     // Send the report to Slack
     await axios.post(slackWebhookUrl, {
       text: 'Cypress Mochawesome report',
       attachments: [
         {
+          color: 'good',
           title: 'Mochawesome Report',
-          text: report,
+          text: JSON.stringify(report, null, 2),
         },
       ],
     });
